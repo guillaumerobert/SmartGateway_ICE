6aa9e5c41531b99c9e2d0d32c41c2ee11896b7a3
Geoffrey <geoffrey.sentost@gmail.com>
2016-01-11 15:04:18 +0100

    mix facture done

--- SmartGateway_PDC/src/Vue/Facture.java
+++ SmartGateway_PDC/src/Vue/Facture.java
@@ -1,7 +1,18 @@
+/*********************************************************************
+	Rhapsody	: 8.1.3
+	Login		: Guillaume Robert
+	Component	: DefaultComponent
+	Configuration 	: DefaultConfig
+	Model Element	: Facture
+//!	Generated Date	: Mon, 11, Jan 2016 
+	File Path	: DefaultComponent/DefaultConfig/Vue/Facture.java
+*********************************************************************/
+
 package Vue;
 
+//## link itsControleurFournisseur 
+import Controleur.ControleurFournisseur;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.Random;
@@ -9,14 +20,23 @@ import java.util.Random;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+//----------------------------------------------------------------------------
+// Vue/Facture.java                                                                  
+//----------------------------------------------------------------------------
+
+//## package Vue 
+
+
+//## class Facture 
 public class Facture extends JPanel {
-	
-	//private ControleurFournisseur cf;
-	
+    
+    protected ControleurFournisseur itsControleurFournisseur;		//## link itsControleurFournisseur 
+    
 	private JLabel titre,nomCli,conso,compteur,releveHC,releveHP,releveTotal,coutUnitaire,totalTTC,aReglerAvant;
 	private JPanel corpsFacture;
 	
-	
+    // Constructors
+    
 	/** JPanel représentatif d'une facture
 	 * Utilise le controleur fournisseur pour avoir accés a toutes les données necessaires pour établir la facture **/
 	public Facture(/*ControleurFournisseur pcf*/){
@@ -56,5 +76,19 @@ public class Facture extends JPanel {
 		this.add(corpsFacture, BorderLayout.CENTER);
 
 	}
-
+    
+    //## auto_generated 
+    public ControleurFournisseur getItsControleurFournisseur() {
+        return itsControleurFournisseur;
+    }
+    
+    //## auto_generated 
+    public void setItsControleurFournisseur(ControleurFournisseur p_ControleurFournisseur) {
+        itsControleurFournisseur = p_ControleurFournisseur;
+    }
+    
 }
+/*********************************************************************
+	File Path	: DefaultComponent/DefaultConfig/Vue/Facture.java
+*********************************************************************/
+
